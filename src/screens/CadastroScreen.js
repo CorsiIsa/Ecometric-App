8b5c66af106bfeb22160637f5b7ce53b969ab000
CorsiIsa <icorsi15@gmail.com>
2024-11-09 13:43:05 -0300

    add firebase auth

--- src/screens/CadastroScreen.js
+++ src/screens/CadastroScreen.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, Alert } from "react-native";
+import { View, Text, TextInput, Button, Alert, ScrollView } from "react-native";
+import { getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import axios from "axios";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuaTWUZkH4W0TlK0KMrSUThAXGu-TgNfc",
+  authDomain: "ecometric-e1483.firebaseapp.com",
+  projectId: "ecometric-e1483",
+  storageBucket: "ecometric-e1483.firebasestorage.app",
+  messagingSenderId: "973015950805",
+  appId: "1:973015950805:web:266878f753d05d6928495c",
+  measurementId: "G-Z7HBZ5V4FT"
+};
+
 
 const CadastroScreen = ({navigation}) => {
   const [nomeEmpresa, setNomeEmpresa] = useState('');
@@ -8,7 +21,18 @@ const CadastroScreen = ({navigation}) => {
   const [inscricaoEstadual, setInscricaoEstadual] = useState('');
   const [razaoSocial, setRazaoSocial] = useState('');
   const [porte, setPorte] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [estado, setEstado] = useState('');
+  const [contato, setContato] = useState('');
+  const [newUser, setNewUser] = useState(null);
+  const app = initializeApp(firebaseConfig)
+  const auth = getAuth(app)
 
+  
   const handleSubmit = async () => {
 
     try {
@@ -17,7 +41,14 @@ const CadastroScreen = ({navigation}) => {
           nrCnpj: cnpj,
           incricaoEstadual: inscricaoEstadual,
           razaoSocial: razaoSocial,
-          porte: porte
+          porte: porte,
+          email : email,
+          senha: senha,
+          cep : cep,
+          endereco : endereco,
+          bairro : bairro,
+          estado: estado,
+          contato: contato,
         };
   
 
@@ -37,6 +68,14 @@ const CadastroScreen = ({navigation}) => {
       console.log('Status:', response.status);
       console.log('Status texto:', response.statusText);
 
+      createUserWithEmailAndPassword(auth, email, senha)
+      .then(userCredential => {
+        setNewUser(userCredential.user);
+      })
+      .catch(error => {
+        console.error('Erro ao criar usuário:', error);
+      });
+
       Alert.alert('Sucesso', 'Dados cadastrados com sucesso!');
       resetForm();
       navigation.navigate("Login")
@@ -52,43 +91,88 @@ const CadastroScreen = ({navigation}) => {
     setInscricaoEstadual('');
     setRazaoSocial('');
     setPorte('');
+    setEmail('');
+    setSenha('');
+    setCep('');
+    setEndereco('');
+    setBairro('');
+    setEstado('');
+    setEstado('');
   };
 
   return (
-    <View>
-      <Text>Cadastro de Empresa</Text>
-      <TextInput
-        placeholder="Nome da empresa"
-        value={nomeEmpresa}
-        onChangeText={setNomeEmpresa}
-      />
-      
-      <TextInput
-        placeholder="CNPJ"
-        value={cnpj}
-        onChangeText={setCnpj}
-      />
-      
-      <TextInput
-        placeholder="Inscrição Estadual"
-        value={inscricaoEstadual}
-        onChangeText={setInscricaoEstadual}
-      />
-      
-      <TextInput
-        placeholder="Razão Social"
-        value={razaoSocial}
-        onChangeText={setRazaoSocial}
-      />
-      
-      <TextInput
-        placeholder="Porte (Grande/Média/Pequena)"
-        value={porte}
-        onChangeText={setPorte}
-      />
-      
-      <Button title="Cadastrar" onPress={handleSubmit} />
-    </View>
+    <ScrollView>
+      <View>
+        <Text>Cadastro de Empresa</Text>
+        <TextInput
+          placeholder="Nome da empresa"
+          value={nomeEmpresa}
+          onChangeText={setNomeEmpresa}
+        />
+        
+        <TextInput
+          placeholder="CNPJ"
+          value={cnpj}
+          onChangeText={setCnpj}
+        />
+        
+        <TextInput
+          placeholder="Inscrição Estadual"
+          value={inscricaoEstadual}
+          onChangeText={setInscricaoEstadual}
+        />
+        
+        <TextInput
+          placeholder="Razão Social"
+          value={razaoSocial}
+          onChangeText={setRazaoSocial}
+        />
+        
+        <TextInput
+          placeholder="Porte (Grande/Média/Pequena)"
+          value={porte}
+          onChangeText={setPorte}
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder="Senha"
+          value={senha}
+          onChangeText={setSenha}
+        />
+        <TextInput
+          placeholder="cep"
+          value={cep}
+          onChangeText={setCep}
+        />
+        <TextInput
+          placeholder="endereço"
+          value={endereco}
+          onChangeText={setEndereco}
+        />
+        <TextInput
+          placeholder="bairro"
+          value={bairro}
+          onChangeText={setBairro}
+        />
+        <TextInput
+          placeholder="estado"
+          value={estado}
+          onChangeText={setEstado}
+        />
+        <TextInput
+          placeholder="contato"
+          value={contato}
+          onChangeText={setContato}
+        />
+        
+        <Button title="Cadastrar" onPress={handleSubmit} />
+      </View>
+    </ScrollView>
+    
   );
 };
 
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,30 +1,52 @@
 import { SafeAreaView, ScrollView, View, Text, TextInput, Button } from "react-native"
+import { getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { useState } from "react";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuaTWUZkH4W0TlK0KMrSUThAXGu-TgNfc",
+  authDomain: "ecometric-e1483.firebaseapp.com",
+  projectId: "ecometric-e1483",
+  storageBucket: "ecometric-e1483.firebasestorage.app",
+  messagingSenderId: "973015950805",
+  appId: "1:973015950805:web:266878f753d05d6928495c",
+  measurementId: "G-Z7HBZ5V4FT"
+};
+
 const LoginScreen = ({navigation}) =>{
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [newUser, setNewUser] = useState(null);
+
+    const handleLogin = () => {
+      signInWithEmailAndPassword(auth, email, senha)
+        .then(userCredential => {
+          setNewUser(userCredential.user);
+          setEmail('');
+          setSenha('');
+          console.error('Deu certo');
+        })
+        .catch(error => {
+          console.error('Erro ao fazer login:', error);
+        });
+    };
     return (
         <SafeAreaView>
             <ScrollView>
                 <View>
-                    <Text>Nome da Empresa</Text>
-                    <TextInput
-                      placeholder="Nome da empresa"
-                    />
-                    <Text>Nº CNPJ</Text>
-                    <TextInput
-                      placeholder="Número CNPJ"
-                    />
-                    <Text>Inscrição Estadual</Text>
-                    <TextInput
-                      placeholder="Inscrição Estadual"
-                    />
-                    <Text>Razão Social</Text>
-                    <TextInput
-                      placeholder="Razão Social"
-                    />
-                    <Text>Porte</Text>
-                    <TextInput
-                      placeholder="Porte Grande/Média/Pequena"
-                    />
-                    <Button title="Login" onPress={() => navigation.navigate("Cadastro")}/>
+                <TextInput
+                  placeholder="Email"
+                  value={email}
+                  onChangeText={setEmail}
+                />
+                <TextInput
+                  placeholder="Senha"
+                  value={senha}
+                  onChangeText={setSenha}
+                />
+                    <Button title="Login" onPress={handleLogin}/>
                 </View>
             </ScrollView>
         </SafeAreaView>
